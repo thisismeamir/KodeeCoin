bbf7bc1e221c968badc339dbf0a0d2f4c76b4bd9
Kid A <me@thisismeamir.com>
2024-08-05 23:31:28 +0330

    [Implementation] Simple Block

---
+++ src/main/kotlin/blocks/Block.kt
@@ -0,0 +1,20 @@
+package com.thisismeamir.kodeecoin.blocks
+
+import com.thisismeamir.kodeecoin.utils.takeHash
+import java.time.Instant
+
+data class Block(
+    val previousHash: String,
+    val data: String,
+    val timestamp: Long = Instant.now().toEpochMilli(),
+    val salt: Long = 0L,
+    var hash: String = ""
+){
+    init {
+        this.hash = this.blockHash()
+    }
+
+    fun blockHash(): String{
+        return "${previousHash}${data}${timestamp}".takeHash()
+    }
+}
